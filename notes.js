5cfee4e46c83ca7a7e0bc33a0ff63f597ca59b4f
Jefta Dirksen <jeftadirksen@gmail.com>
2018-08-09 19:39:37 +0200

    ToDo description change

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@
 
 ToDo:
 - Refactor jobQueue / jobs
-- jobs: 2 tasks with each a target and action (and resourceType?)
+- jobs: 2 (or multiple) tasks with each a target and action (and resourceType?)
 - Mineral harvest should unload all first
 - Guard job
 
