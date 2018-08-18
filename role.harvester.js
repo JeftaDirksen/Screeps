0dd7858f4ebc364f037de1cbd8474468bb4c0588
Jefta Dirksen <jeftadirksen@gmail.com>
2018-08-18 20:52:31 +0200

    Refactor role harvester

--- role.harvester.js
+++ role.harvester.js
@@ -22,43 +22,40 @@ module.exports = function (creep) {
 		let target = null;
 		
 		// Find container/link
-		if(!target) {
-			let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-				filter: s => (
-					s.structureType == STRUCTURE_CONTAINER
-					|| s.structureType == STRUCTURE_LINK
-				)
-				&& (
-					_.sum(s.store) < s.storeCapacity
-					|| s.energy < s.energyCapacity
-				)
-			});
-			if(container) target = container;
+		let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+			filter: s => (
+				s.structureType == STRUCTURE_CONTAINER
+				|| s.structureType == STRUCTURE_LINK
+			)
+			&& (
+				_.sum(s.store) < s.storeCapacity
+				|| s.energy < s.energyCapacity
+			)
+		});
+		if(container) {
+			let r = creep.transfer(container, RESOURCE_ENERGY);
+			if(r == ERR_NOT_IN_RANGE) creep.goTo(container);
+			return;
 		}
 		
 		// Find other (spawn/extensions/storage)
-		if(!target) {
-			let other = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-				filter: s => (
-					s.structureType == STRUCTURE_SPAWN
-					|| s.structureType == STRUCTURE_EXTENSION
-					|| s.structureType == STRUCTURE_STORAGE
-					) && (
-						s.energy < s.energyCapacity
-						|| _.sum(s.store) < s.storeCapacity
-					)
-			});		
-			if(other) target = other;
-		}
-		
-		// Unload at target
-		if(target) {
-			if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.goTo(target);
-			}
+		let other = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+			filter: s => (
+				s.structureType == STRUCTURE_SPAWN
+				|| s.structureType == STRUCTURE_EXTENSION
+				|| s.structureType == STRUCTURE_STORAGE
+				) && (
+					s.energy < s.energyCapacity
+					|| _.sum(s.store) < s.storeCapacity
+				)
+		});		
+		if(other) {
+			let r = creep.transfer(other, RESOURCE_ENERGY);
+			if(r == ERR_NOT_IN_RANGE) creep.goTo(other);
+			return;
 		}
 		
 		// Drop
-		else creep.drop(RESOURCE_ENERGY);
+		creep.drop(RESOURCE_ENERGY);
 	}
 }
