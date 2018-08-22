47cedd75d58c5786a8918e7541bcbde2a25403bc
Jefta Dirksen <jeftadirksen@gmail.com>
2018-08-22 19:07:13 +0200

    Repair in hitpoint steps, ramparts first

--- role.repairer.js
+++ role.repairer.js
@@ -23,26 +23,54 @@ module.exports = function (creep) {
 		});
 		if(structure) {
 			creep.goRepair(structure);
+			return;
 		}
 		
-		else {
-			// Repair Rampart/Wall
-			let structures = creep.room.find(FIND_STRUCTURES, {
-				filter: s =>
-					(
-						s.structureType == STRUCTURE_WALL
-						|| s.structureType == STRUCTURE_RAMPART
-					)
-					&& s.hits < s.hitsMax
-			});
-			let structure = _.sortBy(structures, 'hits')[0];
-			if (structure) {
-				creep.goRepair(structure);
-			}
-			
-			// Idle
-			else creep.goIdle();
+		// Get all not full health Rampart/Wall
+		let structures = creep.room.find(FIND_STRUCTURES, {
+			filter: s =>
+				(
+					s.structureType == STRUCTURE_WALL
+					|| s.structureType == STRUCTURE_RAMPART
+				)
+				&& s.hits < s.hitsMax
+		});
+		if(!structures) {
+			creep.goIdle();
+			return;
 		}
+		
+		// Repair in hitpoint steps
+		let stepSize = 1000;
+		for(let h = stepSize; h <= 300000000; h += stepSize) {
+			let thisStepStructures = _.filter(structures, function(s) {
+				return s.hits < h;
+			});
+			if(thisStepStructures.length) {
+				
+				// Rampart
+				let rampart = creep.pos.findClosestByPath(thisStepStructures, {
+					filter: {structureType: STRUCTURE_RAMPART}
+				});
+				if(rampart) {
+					creep.goRepair(rampart);
+					return;
+				}
+				
+				// Other
+				let other = creep.pos.findClosestByPath(thisStepStructures);
+				if(other) {
+					creep.goRepair(other);
+					return;
+				}
+				
+				break;
+			}
+		}
+
+		// Idle
+		creep.goIdle();
+		
 	}
 	
 	// Load
