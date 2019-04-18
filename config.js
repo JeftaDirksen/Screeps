885691b5027d4b1fbf564d7228db7b39e1f2cb16
Jefta Dirksen <jeftadirksen@gmail.com>
2019-04-18 19:46:29 +0200

    Creep types

--- config.js
+++ config.js
@@ -3,22 +3,19 @@ module.exports = {
         type: {
             a: [
                 {cost:  250, body: [MOVE, WORK, WORK]},
-                {cost:  250, body: [MOVE, WORK, WORK, WORK]},
-                {cost:  250, body: [MOVE, WORK, WORK]},
-                {cost:  250, body: [MOVE, WORK, WORK]},
+                {cost:  500, body: [MOVE, MOVE, WORK, WORK, WORK, WORK]},
+                {cost: 1000, body: [MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK]},
             ],
             b: [
                 {cost:  200, body: [MOVE, MOVE, CARRY, CARRY]},
                 {cost:  400, body: [MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY]},
                 {cost:  800, body: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY]},
-                {cost: 1600, body: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY]},
             ],
             c: [
-              {cost:  250, body: [MOVE, MOVE, CARRY, WORK]},
-              {cost:  400, body: [MOVE, MOVE, MOVE, CARRY, WORK, WORK]},
-              {cost:  500, body: [MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, WORK, WORK]},
-              {cost: 1000, body: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK]},
-              {cost: 1750, body: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK, WORK, WORK, WORK]},
+                {cost:  250, body: [MOVE, MOVE, CARRY, WORK]},
+                {cost:  400, body: [MOVE, MOVE, MOVE, CARRY, WORK, WORK]},
+                {cost:  500, body: [MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, WORK, WORK]},
+                {cost: 1000, body: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK]},
             ],
             d: [
                 {cost: 900, body: [MOVE, MOVE, MOVE, CARRY, WORK, CLAIM]},
