a9651958963c5063a03c55c195c325a3ed60e8e3
Jefta Dirksen <jeftadirksen@gmail.com>
2019-07-15 21:10:48 +0200

    Links: sender and receivers instead of balancing

--- link.js
+++ link.js
@@ -1,31 +1,53 @@
 var f = require('functions');
 
 module.exports = function () {
+    // Memory setup
+	if(!Memory.links) Memory.links = {};
+    
 	// For every room
 	for (let roomName in Game.rooms) {
 		let room = Game.rooms[roomName];
 
-		// Get all active links
+		// Check links for undefined type
 		let links = room.find(FIND_MY_STRUCTURES, {
-			filter: s => s.structureType == STRUCTURE_LINK
+			filter: s =>
+			    s.structureType == STRUCTURE_LINK
+			    && (!Memory.links[s.id] || !Memory.links[s.id].type)
 		});
-		if (!links.length) continue;
-		
-		// Links statistics
-		links = _.sortBy(links, 'energy');
-		let linksCount = links.length;
-		let linkMin = links[0];
-		let linkMax = links[linksCount-1];
-		if(linkMax.cooldown) continue;
-		let linksEnergyTotal = _.sum(links, function(o){return o.energy;});
-		let linksEnergyBalance = Math.round(linksEnergyTotal / linksCount);
-		
-		// Transfer from max to min, amount: above balance, with minimum of 50
-		let amount = linkMax.energy - linksEnergyBalance;
-		if (amount >= 50) {
-			linkMax.transferEnergy(linkMin, amount);
-			//f.debug('Link transfer '+amount+ ' '+linksEnergyBalance);
+		for(let i in links) {
+		    let link = links[i];
+		    if(!Memory.links[link.id]) Memory.links[link.id] = {};
+		    Memory.links[link.id].type = 'sender';
 		}
+
+		// Senders with energy and no cooldown
+		let senders = room.find(FIND_MY_STRUCTURES, {
+			filter: s =>
+			    s.structureType == STRUCTURE_LINK
+			    && (Memory.links[s.id].type == 'sender')
+			    && s.energy > 0
+			    && s.cooldown == 0
+		});
 		
+		// Receivers not full
+		let receivers = room.find(FIND_MY_STRUCTURES, {
+			filter: s =>
+			    s.structureType == STRUCTURE_LINK
+			    && (Memory.links[s.id].type == 'receiver')
+			    && s.energy < s.energyCapacity
+		});
+
+        // Transfer from highestSender to lowestReceiver
+        if(senders.length && receivers.length) {
+            // Highest sender
+            senders = _.sortByOrder(senders, 'energy', 'desc');
+            let highestSender = senders[0];
+
+    		// Lowest receiver
+    		receivers = _.sortBy(receivers, 'energy');
+    		let lowestReceiver = receivers[0];
+
+            highestSender.transferEnergy(lowestReceiver);
+        }
 	}
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,7 @@
 /*
 
 ToDo:
+    x Links: sender and receivers instead of balancing
     - Split transporter into supplier / collector
 
 */
