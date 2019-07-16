7b70eb0c556151a7dc5f6d7761ddfeb09b8d1d68
Jefta Dirksen <jeftadirksen@gmail.com>
2019-07-16 19:44:25 +0200

    tower optimization

--- tower.js
+++ tower.js
@@ -6,12 +6,23 @@ module.exports = function () {
 		let room = Game.rooms[roomName];
 		
 		// Memory setup
-		if(room.memory.wallRepairSpeed == undefined) room.memory.wallRepairSpeed = 'slow';	// slow, medium, fast
+		if (room.memory.wallRepairSpeed == undefined) room.memory.wallRepairSpeed = 'slow';	// slow, medium, fast
+		if (!Memory.towers) Memory.towers = {};
 		
 		let towers = Game.rooms[roomName].find(FIND_STRUCTURES, {
 			filter: s => s.structureType == STRUCTURE_TOWER
 		});
 		for (let tower of towers) {
+		    
+		    // Tower memory
+			if (!Memory.towers[tower.id]) Memory.towers[tower.id] = {}
+
+            // Check pause
+            if (Memory.towers[tower.id].pause) {
+                Memory.towers[tower.id].pause = false;
+                continue;
+            }
+
 			// Attack healers
 			let healer = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
 				filter: function (hostile) {
@@ -20,54 +31,62 @@ module.exports = function () {
 			});
 			if (healer != undefined) {
 				tower.attack(healer);
+				Memory.towers[tower.id].pause = true;
 				return;
 			}
 			
 			// Attack hostiles
-			let target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-			if (target) tower.attack(target);
+			let hostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+			if (hostile) {
+			    tower.attack(hostile);
+				Memory.towers[tower.id].pause = true;
+			    return;
+			}
 			
-			else {
-				// Heal my creeps
-				let target = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-					filter: c => c.hits < c.hitsMax
-				});
-				if (target) tower.heal(target);
+			// Heal my creeps
+			let healCreep = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+				filter: c => c.hits < c.hitsMax
+			});
+			if (healCreep) {
+			    tower.heal(healCreep);
+				Memory.towers[tower.id].pause = true;
+			    return;
+			}
 
-				else {
-					// Repair
-					let speed = 15;
-					if(room.memory.wallRepairSpeed == 'medium') speed = 8;
-					else if(room.memory.wallRepairSpeed == 'fast') speed = 1;
-					
-					let target = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-						filter: s =>
-							s.structureType != STRUCTURE_WALL
-							&& s.structureType != STRUCTURE_RAMPART
-							&& s.hits < s.hitsMax
-					});
-					if (target) {
-						tower.repair(target);
-					}
-					
-					else if (!(Game.time % speed)) {
-						// Repair Rampart/Wall
-						let targets = tower.room.find(FIND_STRUCTURES, {
-							filter: s =>
-								(
-									s.structureType == STRUCTURE_WALL
-									|| s.structureType == STRUCTURE_RAMPART
-								)
-								&& s.hits < s.hitsMax
-						});
-						let target = _.sortBy(targets, 'hits')[0];
-						if (target) {
-							tower.repair(target);
-						}
-						
-					}
+			// Repair
+			let repairTarget = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+				filter: s =>
+					s.structureType != STRUCTURE_WALL
+					&& s.structureType != STRUCTURE_RAMPART
+					&& s.hits < s.hitsMax
+			});
+			if (repairTarget) {
+				tower.repair(repairTarget);
+				Memory.towers[tower.id].pause = true;
+				return;
+			}
+			
+			// Repair Rampart/Wall
+			let speed = 8;
+			if (room.memory.wallRepairSpeed == 'medium') speed = 4;
+			else if (room.memory.wallRepairSpeed == 'fast') speed = 1;
+			if (!(Game.time % speed)) {
+				let targets = tower.room.find(FIND_STRUCTURES, {
+					filter: s =>
+						(
+							s.structureType == STRUCTURE_WALL
+							|| s.structureType == STRUCTURE_RAMPART
+						)
+						&& s.hits < s.hitsMax
+				});
+				let target = _.sortBy(targets, 'hits')[0];
+				if (target) {
+					tower.repair(target);
+    				Memory.towers[tower.id].pause = true;
+					return;
 				}
 			}
+			
 		}
 	}
 };
