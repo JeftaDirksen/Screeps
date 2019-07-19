38b695e1c943343f8a848f4556e0752f6437a9d9
Jefta Dirksen <jeftadirksen@gmail.com>
2019-07-19 08:08:10 +0200

    Fix: 'type' of undefined

--- role.supplier.js
+++ role.supplier.js
@@ -92,7 +92,7 @@ module.exports = function (creep) {
             jobTarget = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 filter: s =>
                     s.structureType == STRUCTURE_LINK
-                    && Memory.links[s.id].type == 'sender'
+                    && (Memory.links[s.id] && Memory.links[s.id].type == 'sender')
                     && s.energy < s.energyCapacity
             });
             if(jobTarget) creep.memory.jobTarget = jobTarget.id;
