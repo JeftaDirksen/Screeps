456e451e686e5476e9589bccb75ee6a87f0a1a63
Jefta Dirksen <jeftadirksen@gmail.com>
2019-10-20 11:16:57 +0200

    Stop upgrading controller @ lvl 8

--- spawn.js
+++ spawn.js
@@ -20,6 +20,7 @@ module.exports = function () {
         let upgradersNeeded = 2;
         if (spawn.room.controller.level == 1) upgradersNeeded = 1;
         if (spawn.room.controller.level == 2) upgradersNeeded = 4;
+        if (spawn.room.controller.level == 8) upgradersNeeded = 0;
         if (spawn.room.countCreeps("upgrader") < upgradersNeeded) {
             const type = 'upgrader';
             const name = spawn.generateCreepName(type);
