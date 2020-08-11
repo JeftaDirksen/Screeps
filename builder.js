92bcbabc859b987aad971605f2e489db5b0d8402
Jefta Dirksen <JeftaDirksen@gmail.com>
2020-08-11 20:45:38 +0200

    Builder idle

--- builder.js
+++ builder.js
@@ -24,11 +24,15 @@ function run(creep) {
         if(site) {
             if (creep.build(site) == ERR_NOT_IN_RANGE) creep.goTo(site);
         }
+        else {
+            creep.idle();
+        }
     }
     
     // Get energy
-    else {
-        if ( !creep.getEnergy() && creep.store[RESOURCE_ENERGY]) creep.memory.build = true;
+    else if(!creep.getEnergy()) {
+        if(creep.store[RESOURCE_ENERGY]) creep.memory.build = true;
+        creep.idle();
     }
 
 }
