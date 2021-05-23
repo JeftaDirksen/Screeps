84acdc29e5f4843c33b617c9c633e30fda3b5e3c
Jefta Dirksen <JeftaDirksen@gmail.com>
2021-05-23 10:37:30 +0200

    Towers attack/heal only

--- tower.js
+++ tower.js
@@ -5,13 +5,8 @@ module.exports = function () {
 		const towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {
 			filter: s => s.structureType == STRUCTURE_TOWER
 		});
-		let towerCount = towers.length;
-		
-		for (let t = 0; t < towerCount; t++) {
-		    // Check if it is this towers turn to fire
-		    //if ((Game.time + t) % towerCount) continue;
-		    
-		    let tower = towers[t];
+		for (let t = 0; t < towers.length; t++) {
+		    const tower = towers[t];
 
 			// Attack healers
 			let healer = tower.pos.findInRange(FIND_HOSTILE_CREEPS, 5, {
@@ -39,37 +34,7 @@ module.exports = function () {
 			    tower.heal(healCreep);
 			    continue;
 			}
-
-			// Repair
-			let repairTarget = tower.pos.findInRange(FIND_STRUCTURES, 5, {
-				filter: s =>
-					s.structureType != STRUCTURE_WALL
-					&& s.structureType != STRUCTURE_RAMPART
-					&& s.hits < s.hitsMax
-			})[0];
-			if (repairTarget) {
-				tower.repair(repairTarget);
-				continue;
-			}
 			
-			// Repair Rampart/Wall
-			//if ( (Game.time % 5) ) continue;
-			//if (tower.energy < .9*tower.energyCapacity) continue;
-			let targets = tower.pos.findInRange(FIND_STRUCTURES, 5, {
-				filter: s =>
-					(
-						s.structureType == STRUCTURE_WALL
-						|| s.structureType == STRUCTURE_RAMPART
-					)
-					&& s.hits < s.hitsMax
-					&& s.hits < (room.controller.level - 2) * 10000000 / 6
-			});
-			let target = _.sortBy(targets, 'hits')[0];
-			if (target) {
-				tower.repair(target);
-				continue;
-			}
-				
 		}
 	}
 }
